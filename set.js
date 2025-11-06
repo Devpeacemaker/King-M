0a0078b77ad4d9fb94d5b381b3a148bda3038652
mesuit <makatech92@gmail.com>
2025-11-06 15:28:12 +0300

    Update set.js

--- set.js
+++ set.js
@@ -1,15 +1,13 @@
-
 const sessionName = 'session';
 const session = process.env.SESSION || '';
 const appname = process.env.APP_NAME || '';
 const herokuapi = process.env.HEROKU_API;
-const botname = process.env.BOTNAME || 'ᴘᴇᴀᴄᴇ-ʜᴜʙ';
+const botname = process.env.BOTNAME || 'KING-M';
 const author = process.env.STICKER_AUTHOR || 'ᴄᴏʀᴇ';
 const packname = process.env.STICKER_PACKNAME || 'ᴘᴇᴀᴄᴇ';
 const dev = process.env.DEV || '254769995625';
 const owner = dev.split(",");
-const menulink = process.env.MENU_LINK || 'https://files.catbox.moe/as1b4c.png
-';
+const menulink = process.env.MENU_LINK || 'https://files.catbox.moe/as1b4c.png';
 const menu = process.env.MENU_TYPE || 'VIDEO';
 const bad = process.env.BAD_WORD || 'fuck';
 const admin = process.env.ADMIN_MSG || 'ᴄᴏᴍᴍᴀɴᴅ ʀᴇsᴇʀᴠᴇᴅ ꜰᴏʀ ᴀᴅᴍɪɴs!';
